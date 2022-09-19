72604efc2a5aef27971e40f17ac2aa5a7c4c9d79
karol-szczawinski <karol.szczawinski.it@gmail.com>
2022-09-19 18:05:30 +0200

    swashbuckle added

--- appointment-manager/Vendor/Vendor.Api/Program.cs
+++ appointment-manager/Vendor/Vendor.Api/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Vendor API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 app.UseRouting();
-app.UseEndpoints(endpoints => endpoints.MapControllers());
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapSwagger();
+});
+
+app.UseSwaggerUI(swagger =>
+{
+    swagger.SwaggerEndpoint("v1/swagger.json", "Vednor API v1");
+});
 
 app.Run();
