990e0f31c749c6247a63de24c988567b8f818696
karol-szczawinski <karol.szczawinski.it@gmail.com>
2022-09-19 17:47:44 +0200

    store controller mock

---
+++ appointment-manager/Vendor/Vendor.Api/Controllers/StoreController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vendor.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class StoreController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult MyStore()
+        {
+            return Ok("success");
+        }
+    }
+}
